b0049cf941affc38dd04f8fd1ca25602eca33065
Boris Berman <bermanboris08@gmail.com>
2024-12-05 21:51:22 +0000

    solve day 4

--- day-04.py
+++ day-04.py
@@ -31,3 +31,40 @@ for r in range(rows):
                 count += 1
 
 print(count)
+
+# 
+# M.S
+# .A. 
+# M.S 
+# 
+# S.S
+# .A. 
+# M.M 
+# 
+# M.M
+# .A. 
+# S.S 
+# 
+# S.M
+# .A. 
+# S.M
+def is_real_xmas(r,c):
+    if (r == 0 and c == 0): print(lines[r][c:c+3])
+    if lines[r][c] == "M" and lines[r][c +2] == "S" and lines[r + 1][c +1] == "A" and lines[r + 2][c] == "M" and lines[r +2][c +2] == "S":
+        return True
+    if lines[r][c] == "S" and lines[r][c +2] == "S" and lines[r + 1][c +1] == "A" and lines[r + 2][c] == "M" and lines[r +2][c +2] == "M":
+        return True
+    if lines[r][c] == "M" and lines[r][c +2] == "M" and lines[r + 1][c +1] == "A" and lines[r + 2][c] == "S" and lines[r +2][c +2] == "S":
+        return True
+    if lines[r][c] == "S" and lines[r][c +2] == "M" and lines[r + 1][c +1] == "A" and lines[r + 2][c] == "S" and lines[r +2][c +2] == "M":
+        return True
+    return False
+
+real_xmas = 0
+for r in range(rows):
+    for c in range(cols):
+        if r + 2 < rows and c + 2 < cols:
+            if is_real_xmas(r,c):
+                real_xmas += 1
+print(real_xmas)
+
