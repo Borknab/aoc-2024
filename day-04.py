fec1bac0ce65a2d52c428c8c42c120ecb77f81c9
Boris Berman <bermanboris08@gmail.com>
2024-12-05 13:02:02 +0000

    smallfix

--- day-04.py
+++ day-04.py
@@ -2,7 +2,6 @@ f = open("input-04.txt", "r")
 lines = []
 for l in f: lines.append(l.strip())
 
-word = "XMAS"
 rows = len(lines)
 cols = len(lines[0])
 count = 0
@@ -18,17 +17,17 @@ directions = [
     (1, 1) 
 ]
 
-def find_word(r, c, dy, dx):
+def find_xmas(r, c, dy, dx):
     for i in range(4):
         nr, nc = r + i * dy, c + i * dx
-        if nr < 0 or nr >= rows or nc < 0 or nc >= cols or lines[nr][nc] != word[i]:
+        if nr < 0 or nr >= rows or nc < 0 or nc >= cols or lines[nr][nc] != "XMAS"[i]:
             return False
     return True
 
 for r in range(rows):
     for c in range(cols):
         for dy, dx in directions:
-            if find_word(r, c, dy, dx):
+            if find_xmas(r, c, dy, dx):
                 count += 1
 
 print(count)
